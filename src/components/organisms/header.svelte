4d738f50a00f94ff8e9e1fd11492ef50666f7543
Jeremy Lanssiers <jeremy.lanssiers@gmail.com>
2023-08-15 22:18:34 +0200

    main - SvelteKit app

---
+++ src/components/organisms/header.svelte
@@ -0,0 +1,18 @@
+<script>
+	import Navigation from '../molecules/navigation.svelte';
+</script>
+
+<header>
+	<Navigation />
+</header>
+
+<style lang="scss">
+	@import '../../styles/variables.scss';
+
+	header {
+		margin-bottom: $header-and-footer-margin;
+		padding: $header-and-footer-padding;
+		height: $header-and-footer-height;
+		width: calc(100% - 2 * #{$header-and-footer-padding});
+	}
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,58 @@
+<script>
+	import Header from '../components/organisms/header.svelte';
+	import Footer from '../components/organisms/footer.svelte';
+</script>
+
+<Header />
+<main>
+	<slot />
+</main>
+<Footer />
+
+<style lang="scss">
+	@import '../styles/variables.scss';
+
+	:global(*) {
+		margin: 0;
+		border: 0;
+		padding: 0;
+	}
+
+	:global(body) {
+		background-color: $background-color;
+		color: $color;
+		font-family: sans-serif;
+		font-size: 1rem;
+	}
+
+	:global(main) {
+		display: flex;
+		flex-direction: column;
+		flex-grow: 1;
+		justify-content: center;
+		margin: 0 auto;
+		padding: $main-padding;
+		min-height: calc(
+			100vh - 2 * #{$header-and-footer-height} - 2 * #{$header-and-footer-padding} - 2 * #{$header-and-footer-margin} -
+				2 * #{$main-padding}
+		);
+		width: 100%;
+		max-width: calc(100vw - 2 * 1.5rem);
+		overflow-x: hidden;
+
+		@media (min-width: 768px) {
+			max-width: calc(768px - 2 * 1.5rem);
+		}
+	}
+
+	:global(h1) {
+		font-size: 2rem;
+		text-transform: uppercase;
+		padding: 1em 0 0.5em;
+	}
+
+	:global(p) {
+		font-family: serif;
+		padding: 1em 0 0.5em;
+	}
+</style>
