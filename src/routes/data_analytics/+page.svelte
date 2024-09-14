644db93d95ebc8677c08f1cedeae0e7f1455935f
crusem <143424983+crusem@users.noreply.github.com>
2024-09-14 17:33:28 -0400

    Update +page.svelte to reflect count bulletin

--- src/routes/data_analytics/+page.svelte
+++ src/routes/data_analytics/+page.svelte
@@ -6,42 +6,26 @@
   }
 </style>
 
-<h1>Data Analytics: Work Samples</h1>
+<h1>DVRPC Pedestrian Count Bulletin</h1>
 <p>
-Delve into a selection of assignments showcasing my skills developed during the first semester of the Master of Urban Spatial Analytics program. While these assignments are not complete projects, they serve as a testament to my proficiency in R and Python. The showcased work reflects foundational knowledge in areas such as machine learning, resource allocation, cost-benefit analysis, data visualization, and predictive modeling.
+  Explore insights from the Pedestrian Count Bulletin series, investigating what variables shape pedestrian activity in the greater Philadelphia region. I conducted this analysis during my time at the Delaware Valley Regional Planning Commission as a transportation planning intern. </p>
+<p>
+<strong>Tools Used:</strong> R, ArcGIS Pro.
 </p>
 
-<h2>Exploring Evictions and Code Violations & Comparing the NDVI in Philadelphia, PA</h2>
+<h2><strong>#1 Behind the Data:</strong> Uncovering the Impact of College Population and Transit Access on Philadelphia's Pedestrian Activity</h2>
 <p>
-This project was submitted during the 2023 Fall semester for Geospatial Data Science in Python at the Weitzman School of Design at the University of Pennsylvania.
+This is the first article in a series of four data bulletins that analyze whether and how different variables affect pedestrian counts. Using data from DVRPC’s cyclical pedestrian count program, planners looked at how college population and transit access may impact pedestrian activity in the City of Philadelphia. It was found that college population and transit activity density significantly influence pedestrian counts, confirming that stratifying census tracts based on these factors effectively captures variations in pedestrian activity levels.
+<a href="https://dvrpc.org/news/2024/pedestrian-data-bulletin-philadelphia/" target="_blank">Read the Article Here!</a> </p>
 
-The objective of the first part of this assignment was to create vizualizations displaying eviction and code violation data in Philadelphia, PA. The objective of the second part of the assignment is to calculate and compare the NDVi in the suburbs and city of Philadelphia, PA.
-<a href="https://crusem.github.io/assignment_sample_4/" target="_blank">View the Submission Here!</a> </p>
-
-<h2>Vizualizing Arrest Data in Philadelphia, PA Using Python</h2>
+<h2>Bulletin #2</h2>
 <p>
-This project was submitted by Marissa Cruse and Alyssa Felix-Arreola during the 2023 Fall semester for Geospatial Data Science in Python at the Weitzman School of Design at the University of Pennsylvania.
+Coming soon!</p>
 
-The objective of this assignment was to gain familiarity using the altair, seaborn, and matplotlib packages in python to create interesting and interactive data vizualizations.
-<a href="https://crusem.github.io/assignment_sample_1/" target="_blank">View the Submission Here!</a> </p>
-
-<h2>Street Networks and Webscraping Using Python</h2>
+<h2>Bulletin #3</h2>
 <p>
-This project was submitted by Marissa Cruse during the 2023 Fall semester for Geospatial Data Science in Python at the Weitzman School of Design at the University of Pennsylvania.
+Coming soon!</p>
 
-The objective of the first part of the assignment was to create vizualizations to display crash incidence data to a street network. The objective of the second part of the assignment was to use web scraping to extract craigslist apartment listing data and create vizualizations.
-<a href="https://crusem.github.io/assignment_sample_2/" target="_blank">View the Submission Here!</a> </p>
-
-<h2>Targeting a Housing Subsidy</h2>
+<h2>Bulletin #4</h2>
 <p>
-This project was submitted by Marissa Cruse and Alyssa Felix-Arreola during the 2023 Fall semester for Public Policy Analytics at the Weitzman School of Design at the University of Pennsylvania.
-
-The objective of this assignment was to train the best classifier and use the results to inform a cost/benefit analysis for a housing subsidy using R.
-<a href="https://crusem.github.io/assignment_sample_3/" target="_blank">View the Submission Here!</a> </p>
-
-<h2>Exploring Yelp Reviews in Philadelphia Using Python</h2>
-<p>
-This project was submitted during the 2023 Fall semester for Geospatial Data Science in Python at the Weitzman School of Design at the University of Pennsylvania.
-
-The first objective of this assignment was to analyze the correlation between restaurant reviews and income. The second objective was to conducting a sentiment analysis on resturaunt reviews.
-<a href="https://crusem.github.io/assignment_sample_5/" target="_blank">View the Submission Here!</a> </p>
+Coming soon!</p>
