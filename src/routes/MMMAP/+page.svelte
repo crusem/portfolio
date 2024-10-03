a0ece80547edfba01271ea18d9c508a9ab5200cf
crusem <143424983+crusem@users.noreply.github.com>
2024-10-03 16:44:35 -0400

    Update +page.svelte

--- src/routes/MMMAP/+page.svelte
+++ src/routes/MMMAP/+page.svelte
@@ -23,6 +23,6 @@
   To begin making a user's custom map, MMMap utilizes the Spotify API to fetch user's saved tracks. Next, for each artist in a user's library, there is a frequeny value that represents the number of times the artist is represented in the user's library. After using webscraping to extract each artist's birthplace and geocoding the provided birthplace, MMMap creates a heatmap! The MMMap database contains over 84,000 artists provided by Localify, a platform that personalizes local music recommendations by integrating Spotify playlists with tracks from local artists. Each time a new user uses MMMap, the artists they listen to are added to MMMap's  database.
 </p>
 
-<p>
+<p> MMMAP is currently under construction, but if you're curious - 
   <a href="https://mmmapproject.com" target="_blank">Click here to visit the site!</a> 
 </p>
